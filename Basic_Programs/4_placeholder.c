57505c8a554d341da15b324f20c9f23d277b1ad4
UtkarshShah0 <93548048+UtkarshShah0@users.noreply.github.com>
2023-04-01 19:21:31 +0530

    hello world

---
+++ Basic_Programs/4_placeholder.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+
+printf("Hello World\n");
+printf("%d\n",-325444);                 //integer
+printf("%ld\n",222222222);          //large integr
+printf("%f\n",4555654334432.0067);           //float
+printf("%lf\n",34334432564.09876);        //large float
+printf("%c\n",'a' );                   //character
+printf("%s\n","Hello World" );          //string
+
+
+return 0;
+}
